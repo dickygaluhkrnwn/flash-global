b43357ffbb6821b4b23f8b8fc975d02dc5c817a2
dickygaluhkrnwn~ <dicky.galuh.kurniawan1@gmail.com>
2026-07-20 02:11:29 +0800

    Auth domain

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,7 +5,11 @@ import { getFirestore } from "firebase/firestore";
 // Mengambil kunci dari file .env.local
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  
+  // PENTING: Kita ganti authDomain langsung ke subdomain milikmu 
+  // agar layar Login Google 100% menggunakan brand Flash Global Logistik
+  authDomain: "web.flashglobalslogistik.com", 
+  
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
